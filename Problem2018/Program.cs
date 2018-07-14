ff14742f0f5a8d1ec3b6c9c9ae2cfe786f7ce81a
Albin <a.bramstang@gmail.com>
2018-07-14 15:11:26 +0200

    Add project files.

---
+++ Problem2018/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
