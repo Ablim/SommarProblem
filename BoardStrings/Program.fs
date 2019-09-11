e9c6050c1fe0fb7925ebfc32a1c6a69034c29909
Albin <a.bramstang@gmail.com>
2019-09-11 21:56:59 +0200

    Add pseudo code.

--- BoardStrings/Program.fs
+++ BoardStrings/Program.fs
@@ -1,11 +1,36 @@
 ﻿open System
 
-let readInput =
-    
-    ""
 
-let readBoard 
 
 [<EntryPoint>]
 let main argv =
+    printf("Hello")
     0
+
+(*
+    for each string s
+        save s in a map
+    
+    for x = 0..width
+        for y = 0..height
+            for each string s in map
+                if s begins with char @ (x, y)
+                    save (x, y) with s in map
+
+    for each string s in map
+        for each coordinate c with s
+            look for s at c recursively
+
+    rec func(matrix, x, y, [a:as])
+        if !valid coordinates
+            return false
+        
+        if []
+            return true
+
+        if matrix(x, y) != a
+            return false
+        else
+            for each 8 directions dx, dy
+                return true && func(matrix, dx, dy, as)
+*)
