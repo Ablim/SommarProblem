94531255f87c8bf7a6ef9febe82ac14a01981a92
Albin <a.bramstang@gmail.com>
2019-09-24 21:16:37 +0200

    Cleaned. Added test script.

--- BoardStrings/Program.fs
+++ BoardStrings/Program.fs
@@ -58,58 +58,11 @@ let main argv =
     let board = readBoard
     let words = readWordsAsList [] false
 
-    let lala = board.Length
-    let lala2 = Array.length board
-
-
     for w in words do
-        for i = 0 to board.Length do
-            for j = 0 to board.[i].Length do
+        for i = 0 to (board.Length - 1) do
+            for j = 0 to (board.[i].Length - 1) do
                 let exists = search board i j (List.ofSeq w)
                 if exists then
                     printfn "%s" w
     
-    //let apa = "APA"
-    //let apaList = List.ofSeq apa
-    //let apaArray = Array.ofSeq apa
-
-    //let arr1 = [| 'A'; 'P' |]
-    //let arr2 = [| 'B'; 'U' |]
-    //let arr3 = [| 'U' |]
-
-    //let matrix = [| arr1; arr2; arr3|]
-
-    //printfn "%b" (search matrix 0 0 ['A'; 'P'; 'A'])
-
     0
-
-
-
-
-(*
-    for each string s
-        save s in a map
-    
-    for x = 0..width
-        for y = 0..height
-            for each string s in map
-                if s begins with char @ (x, y)
-                    save (x, y) with s in map
-
-    for each string s in map
-        for each coordinate c with s
-            look for s at c recursively
-
-    rec func(matrix, x, y, [a:as])
-        if !valid coordinates
-            return false
-        
-        if []
-            return true
-
-        if matrix(x, y) != a
-            return false
-        else
-            for each 8 directions dx, dy
-                return true && func(matrix, dx, dy, as)
-*)
---
+++ BoardStrings/Test.sh
@@ -0,0 +1,2 @@
+dotnet build
+cat Input.txt | dotnet run BoardStrings.fsproj
